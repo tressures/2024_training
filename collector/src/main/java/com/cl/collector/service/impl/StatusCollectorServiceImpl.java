a4d532244aefd7e9ab3633bd3b6180c92e36a2b4
sunmaolin <3253636937@qq.com>
2024-06-03 23:56:27 +0800

    采集算法优化

--- collector/src/main/java/com/cl/collector/service/impl/StatusCollectorServiceImpl.java
+++ collector/src/main/java/com/cl/collector/service/impl/StatusCollectorServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Service("statusCollectorService")
 @Slf4j
@@ -15,23 +16,21 @@ public class StatusCollectorServiceImpl implements StatusCollectorService {
     @Override
     public Double getCpuUsage() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("/proc/stat"));
-        long idlePart = 0,totalPart = 0;
         String line;
+        long totalTime = 0;
+        long idleTime = 0;
         while ((line = reader.readLine()) != null) {
             if (line.startsWith("cpu ")) {
-                String[] parts = line.trim().split(" ");
-                long user = Long.parseLong(parts[1]);
-                long nice = Long.parseLong(parts[2]);
-                long sys = Long.parseLong(parts[3]);
-                long idle = Long.parseLong(parts[4]);
-                long total = user + nice + sys + idle;
-                idlePart=idle;
-                totalPart=total;
-                break;
+                String[] parts = line.split("\\s+");
+                totalTime = Arrays.stream(parts)
+                        .skip(1)
+                        .mapToLong(Long::parseLong)
+                        .sum();
+                idleTime = Long.parseLong(parts[4]);
             }
         }
         reader.close();
-        return Double.parseDouble(String.format("%.2f",100 * idlePart*1.0 / totalPart));
+        return Double.parseDouble(String.format("%.2f",100 * idleTime*1.0 / totalTime));
     }
 
     @Override
