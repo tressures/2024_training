4ea56540ff573d791c93796f38fbefd13bc0dad2
sunmaolin <3253636937@qq.com>
2024-06-04 10:33:13 +0800

    采集算法优化

--- collector/src/main/java/com/cl/collector/service/impl/StatusCollectorServiceImpl.java
+++ collector/src/main/java/com/cl/collector/service/impl/StatusCollectorServiceImpl.java
@@ -37,18 +37,20 @@ public class StatusCollectorServiceImpl implements StatusCollectorService {
     public Double getMemUsage() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("/proc/meminfo"));
         String line;
-        long totalMemory = 0,freeMemory = 0,availableMemory=0;
+        long totalMemory = 0,freeMemory = 0,Buffers=0,Cached=0;
         while ((line = reader.readLine()) != null) {
             if (line.startsWith("MemTotal:")) {
                 totalMemory = Long.parseLong(line.split("\\s+")[1]);
             } else if (line.startsWith("MemFree:")) {
                 freeMemory = Long.parseLong(line.split("\\s+")[1]);
-            }else if (line.startsWith("MemAvailable :")) {
-                availableMemory = Long.parseLong(line.split("\\s+")[1]);
+            }else if (line.startsWith("Buffers:")) {
+                Buffers = Long.parseLong(line.split("\\s+")[1]);
+            }else if (line.startsWith("Cached:")) {
+                Cached = Long.parseLong(line.split("\\s+")[1]);
             }
         }
         reader.close();
-        return Double.parseDouble(String.format("%.2f",100 * ((totalMemory - freeMemory-availableMemory)*1.0 / totalMemory)));
+        return Double.parseDouble(String.format("%.2f",100 * ((totalMemory - freeMemory - Buffers - Cached)*1.0 / totalMemory)));
     }
 }
 
