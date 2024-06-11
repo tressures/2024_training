0193a947b705fb80758948f5583a8e42cf70d904
sunmaolin <3253636937@qq.com>
2024-06-11 17:02:10 +0800

    配置boot和cloud版本对应

--- server/src/main/java/com/cl/server/config/LogStorageConfig.java
+++ server/src/main/java/com/cl/server/config/LogStorageConfig.java
@@ -1,11 +1,11 @@
 package com.cl.server.config;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+//import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@NacosConfigurationProperties(dataId = "cfg.json", autoRefreshed = true)
+//@NacosConfigurationProperties(dataId = "cfg.json", autoRefreshed = true)
 public class LogStorageConfig {
 
     private static String log_storage;
